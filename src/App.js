af83a4c983165f490bb00ade56a8dba0a8387d59
astudt <andrew_studt@intuit.com>
2018-01-11 14:12:59 -0600

    added redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import configureStore from './store/configure-store';
+import { Provider } from 'react-redux';
+import devConfig from './config/config'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.store = configureStore(devConfig, {});
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={this.store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+// import { get } from 'lodash';
+// import client from './client-reducer';
+
+export default combineReducers({
+});
